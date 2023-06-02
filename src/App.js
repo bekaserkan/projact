e98780271b79a4495224f7bfc2f313301b9025f4
bekaserkan <bekaserkan@gmail.com>
2023-06-02 15:41:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/footer/footer';
 import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function Header () {
     return (
         <div className="header">
-        
+        <h1>Beka</h1>
+        <h3>Ihsan</h3>
         </div>
     )
 }
